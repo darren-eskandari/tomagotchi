5c84af698ddf4d49703fb6148f9c3383365e2220
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-01 13:23:03 -0700

    Class created

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 // Create a Class (JS Class, look at your notes if your forget) for your tomagotchi
 
 class Tamagotchi {
-    constructor(name, gender){
-        
+    constructor(name){
+        this.name = name;
+        this.age = 0;
+        this.hunger = 1;
+        this.sleepiness = 1;
+        this.boredom = 1;
+        this.alive = true;
     }
 }
